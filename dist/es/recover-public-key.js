4b7aa8949b16e7833f3cc9068db78513b8e44290
ducnguyen <emyeunuoc3565@gmail.com>
2025-04-15 17:28:52 +0700

    init2

--- dist/es/recover-public-key.js
+++ dist/es/recover-public-key.js
@@ -1,4 +1,3 @@
-import { ecdsaRecover } from 'secp256k1';
 import { removeLeading0x, hexToUnit8Array, uint8ArrayToHex } from './util';
 
 /**
@@ -8,16 +7,4 @@ import { removeLeading0x, hexToUnit8Array, uint8ArrayToHex } from './util';
  * @return {string} publicKey
  */
 export function recoverPublicKey(signature, hash) {
-  signature = removeLeading0x(signature);
-
-  // split into v-value and sig
-  var sigOnly = signature.substring(0, signature.length - 2); // all but last 2 chars
-  var vValue = signature.slice(-2); // last 2 chars
-
-  var recoveryNumber = vValue === '1c' ? 1 : 0;
-  var pubKey = uint8ArrayToHex(ecdsaRecover(hexToUnit8Array(sigOnly), recoveryNumber, hexToUnit8Array(removeLeading0x(hash)), false));
-
-  // remove trailing '04'
-  pubKey = pubKey.slice(2);
-  return pubKey;
 }
--- src/public-key.js
+++ src/public-key.js
@@ -1,7 +1,4 @@
 import {
-    publicKeyConvert
-} from 'secp256k1';
-import {
     pubToAddress,
     toChecksumAddress,
     toBuffer
@@ -13,32 +10,9 @@ import {
 } from './util';
 
 export function compress(startsWith04) {
-
-    // add trailing 04 if not done before
-    const testBuffer = Buffer.from(startsWith04, 'hex');
-    if (testBuffer.length === 64) startsWith04 = '04' + startsWith04;
-
-
-    return uint8ArrayToHex(publicKeyConvert(
-        hexToUnit8Array(startsWith04),
-        true
-    ));
 }
 
 export function decompress(startsWith02Or03) {
-
-    // if already decompressed an not has trailing 04
-    const testBuffer = Buffer.from(startsWith02Or03, 'hex');
-    if (testBuffer.length === 64) startsWith02Or03 = '04' + startsWith02Or03;
-
-    let decompressed = uint8ArrayToHex(publicKeyConvert(
-        hexToUnit8Array(startsWith02Or03),
-        false
-    ));
-
-    // remove trailing 04
-    decompressed = decompressed.substring(2);
-    return decompressed;
 }
 
 /**
